f528aefa7428423df1c943fe4c910e7d882e6aed
twinaphex <libretro@gmail.com>
2020-10-05 00:53:55 +0200

    Convert fxscsi.cpp to C

--- mednafen/pcfx/fxscsi.c
+++ mednafen/pcfx/fxscsi.c
@@ -15,20 +15,8 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
-#include "pcfx.h"
 #include "fxscsi.h"
 
-bool FXSCSI_Init(void)
-{
-   return true;
-}
-
-uint8 FXSCSI_CtrlRead(uint32 A)
-{
-   return 0;
-}
-
-void FXSCSI_CtrlWrite(uint32 A, uint8 V)
-{
-   //printf("FXSCSI Write: %08x %02x\n", A, V);
-}
+bool FXSCSI_Init(void) { return true; }
+uint8 FXSCSI_CtrlRead(uint32 A) { return 0; }
+void FXSCSI_CtrlWrite(uint32 A, uint8 V) { }
--- mednafen/pcfx/fxscsi.h
+++ mednafen/pcfx/fxscsi.h
@@ -1,6 +1,9 @@
 #ifndef __MDFN_FXSCSI_H
 #define __MDFN_FXSCSI_H
 
+#include <boolean.h>
+#include "../mednafen-types.h"
+
 bool FXSCSI_Init(void);
 uint8 FXSCSI_CtrlRead(uint32 A);
 void FXSCSI_CtrlWrite(uint32 A, uint8 V);
